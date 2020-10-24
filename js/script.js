b7b49e5a85ece64825e4137d0424f3f01bf97751
Rohald van Merode <rohald_89@live.nl>
2020-10-24 23:53:23 +0200

    added addPagination function

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
+const itemsPerPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 const showPage = (list, page) => {
-   const startIndex = (page * 9) - 9;
-   const endIndex = (page * 9) -1;
-   const ul = document.querySelector('.student-list');
-   ul.innerHTML = "";
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = (page * itemsPerPage);
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = "";
+   // loop over all student objects
    list.forEach((student, index) => {
-      console.log(index)
-      if(index >= startIndex && index <= endIndex){
-         console.log('check');
+      // find out wether the student object should be displayed
+      if(index >= startIndex && index < endIndex){
          const li = document.createElement('LI');
          li.className = "student-item cf";
          li.innerHTML = `
@@ -38,18 +38,42 @@ const showPage = (list, page) => {
            <span class="date">${student.registered.date}</span>
          </div>
          `;
-         ul.appendChild(li);
+         studentList.appendChild(li);
       }
    });
 };
-
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+const addPagination = list => {
+   // find out how many pages are needed for all the student cards
+   const pages = Math.ceil(list.length / itemsPerPage);
+   const pagesList = document.querySelector('.link-list');
+   pagesList.innerHTML = "" ;
+   for ( let i = 1 ; i <= pages ; i++) {
+      pagesList.insertAdjacentHTML('beforeend', ` 
+      <li>
+         <button type="button">${i}</button>
+      </li>
+      `);
+   }
+   pagesList.querySelector('button').classList.add('active');
+   pagesList.addEventListener('click', e => {
+      if(e.target.tagName === "BUTTON"){
+         // loop over the buttons to remove the active class
+         pagesList.querySelectorAll('button').forEach(button => {
+            button.classList.remove('active');
+         });
+         e.target.classList.add('active');
+         // load the student cards for the clicked page number
+         showPage(list, e.target.textContent);
+      }
+   });
+};
 
 
 // Call functions
 showPage(data, 1);
+addPagination(data);
