b60cf80bf0b2dfc4e0a8f3f6bfaf4f0c7b21f4d0
Rohald van Merode <rohald_89@live.nl>
2020-10-24 23:14:17 +0200

    added showPage function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,31 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
+const showPage = (list, page) => {
+   const startIndex = (page * 9) - 9;
+   const endIndex = (page * 9) -1;
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = "";
+   list.forEach((student, index) => {
+      console.log(index)
+      if(index >= startIndex && index <= endIndex){
+         console.log('check');
+         const li = document.createElement('LI');
+         li.className = "student-item cf";
+         li.innerHTML = `
+         <div class="student-details">
+           <img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">
+           <h3>${student.name.first} ${student.name.last}</h3>
+           <span class="email">${student.email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">${student.registered.date}</span>
+         </div>
+         `;
+         ul.appendChild(li);
+      }
+   });
+};
 
 
 /*
@@ -28,3 +52,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
